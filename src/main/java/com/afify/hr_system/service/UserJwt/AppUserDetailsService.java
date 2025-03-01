4c46716d9c349df2751dc82d1b450a351aca34d1
Mohamed-Afify <mohamed.org050@gmail.com>
2025-03-01 13:57:37 +0200

    [add] add swigger

--- src/main/java/com/afify/hr_system/service/UserJwt/AppUserDetailsService.java
+++ src/main/java/com/afify/hr_system/service/UserJwt/AppUserDetailsService.java
@@ -29,54 +29,54 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class AppUserDetailsService {
-private final UserRepo userRepo;
-private final UserMapper userMapper;
-private final userPageMapper userPageMapper;
-private final TokenRepo tokenRepo;
-public UserDetails loadByUserName(String userName) {
-	return userRepo.findByEmail(userName);
-}
-public UserPageDto getAllusers(int pageNum,int pageSize){
-	Pageable pageable=PageRequest.of(pageNum, pageSize);
-	Page<AppUser> allUsers=userRepo.findAll(pageable);
-	UserPageDto userPageDto=userPageMapper.map(allUsers);
-	return userPageDto;
-}
-public ResponseEntity<?> updateUser(UserDto user){
-	if(!userRepo.existsById(user.getId())) {
-		throw new RecordNotFoundException("user is not exist");
-	}
-	AppUser appUser=userMapper.unmap(user);
-	userRepo.save(appUser);
-	return ResponseEntity.ok(null);
-}
-public ResponseEntity<?> editUser(UserDto user,Principal connectedUser){
-	AppUser conectUser=(AppUser) ((UsernamePasswordAuthenticationToken)connectedUser).getPrincipal();
-	if(conectUser.getId()!=user.getId()) {
-		throw new RecordNotFoundException("you can only edit your data");
-	}
-	AppUser oldUser=userRepo.findById(user.getId()).get();
-	Role oldRole=oldUser.getRole();
-	byte[] imgBytes=null;
-	if(user.getProfileimg()!=null) {
-		imgBytes=Base64.getDecoder().decode(user.getProfileimg());
-	}
-	String pass=oldUser.getPassword();
-	AppUser appUser=userMapper.unmap(user);
-	appUser.setRole(oldRole);
-	appUser.setPassword(pass);
-	appUser.setImage(imgBytes);
-	UserDto usrDto=userMapper.map(appUser);
-	userRepo.save(appUser);
-	return ResponseEntity.ok(usrDto);
-}
-@Transactional
-public ResponseEntity<?> deleteUser(int id) {
-	if(!userRepo.existsById(id)) {
-		throw new RecordNotFoundException("user is not exist");
-	}
-	tokenRepo.deleteByUserId(id);
-	userRepo.deleteById(id);
-	return ResponseEntity.ok(null);
-}
+    private final UserRepo userRepo;
+    private final UserMapper userMapper;
+    private final userPageMapper userPageMapper;
+    private final TokenRepo tokenRepo;
+    public UserDetails loadByUserName(String userName) {
+        return userRepo.findByEmail(userName);
+    }
+    public UserPageDto getAllusers(int pageNum,int pageSize){
+        Pageable pageable=PageRequest.of(pageNum, pageSize);
+        Page<AppUser> allUsers=userRepo.findAll(pageable);
+        UserPageDto userPageDto=userPageMapper.map(allUsers);
+        return userPageDto;
+    }
+    public ResponseEntity<?> updateUser(UserDto user){
+        if(!userRepo.existsById(user.getId())) {
+            throw new RecordNotFoundException("user is not exist");
+        }
+        AppUser appUser=userMapper.unmap(user);
+        userRepo.save(appUser);
+        return ResponseEntity.ok(null);
+    }
+    public ResponseEntity<?> editUser(UserDto user,Principal connectedUser){
+        AppUser conectUser=(AppUser) ((UsernamePasswordAuthenticationToken)connectedUser).getPrincipal();
+        if(conectUser.getId()!=user.getId()) {
+            throw new RecordNotFoundException("you can only edit your data");
+        }
+        AppUser oldUser=userRepo.findById(user.getId()).get();
+        Role oldRole=oldUser.getRole();
+        byte[] imgBytes=null;
+        if(user.getProfileimg()!=null) {
+            imgBytes=Base64.getDecoder().decode(user.getProfileimg());
+        }
+        String pass=oldUser.getPassword();
+        AppUser appUser=userMapper.unmap(user);
+        appUser.setRole(oldRole);
+        appUser.setPassword(pass);
+        appUser.setImage(imgBytes);
+        UserDto usrDto=userMapper.map(appUser);
+        userRepo.save(appUser);
+        return ResponseEntity.ok(usrDto);
+    }
+    @Transactional
+    public ResponseEntity<?> deleteUser(int id) {
+        if(!userRepo.existsById(id)) {
+            throw new RecordNotFoundException("user is not exist");
+        }
+        tokenRepo.deleteByUserId(id);
+        userRepo.deleteById(id);
+        return ResponseEntity.ok(null);
+    }
 }
