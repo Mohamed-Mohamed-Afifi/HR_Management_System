3f8ccc783891f3a500be6625639800ec84436d78
Mohamed-Afify <mohamed.org050@gmail.com>
2025-03-01 15:33:13 +0200

    [add] add Critera Query to filter users

--- src/main/java/com/afify/hr_system/repo/userAuth/UserRepo.java
+++ src/main/java/com/afify/hr_system/repo/userAuth/UserRepo.java
@@ -7,8 +7,9 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.afify.hr_system.model.appUser.AppUser;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface UserRepo extends JpaRepository<AppUser,Integer>{
+public interface UserRepo extends JpaRepository<AppUser,Integer>, JpaSpecificationExecutor<AppUser> {
 	
 	@EntityGraph(attributePaths = {"tokens"})
 	AppUser findByEmail(String userName);
